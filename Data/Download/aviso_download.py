58b37dc2559c76e870201ce64ca5253c261bb9f8
kalkulator413 <50224596+kalkulator413@users.noreply.github.com>
2023-07-14 15:02:48 -0700

    Update aviso_download.py

--- Data/Download/aviso_download.py
+++ Data/Download/aviso_download.py
@@ -4,8 +4,19 @@ import os
 data_folder = os.path.join(get_data_folder(),'Raw/Aviso/')
 
 def download():
-        for year in range(2012, 2020):
+        for year in range(2017, 2024):
                 for month in range(1, 13):
+
+                        # skip downloading all incomplete months
+                        if year == 2017 and month <= 2:
+                                continue
+                        elif year == 2021 and month == 12:
+                                continue
+                        elif year = 2022 and month == 1:
+                                continue
+                        elif year == 2023 and month > 6:
+                                continue
+
                         url = f"https://coastwatch.noaa.gov/erddap/griddap/noaacwBLENDEDsshDaily.nc?sla%5B({year}-{month}-01):1:({year}-{month}-31T00:00:00Z)%5D%5B(-89.875):1:(89.875)%5D%5B(-179.875):1:(179.875)%5D"
                         filename = str(month) + ".nc"
                         folder = os.path.join(data_folder, str(year))
